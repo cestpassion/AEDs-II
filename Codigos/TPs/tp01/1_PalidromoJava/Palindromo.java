9f2852dac161548ff182393b91c35691e8834750
cestpassion <bffonseca@sga.pucminas.br>
2024-09-02 22:55:24 -0300

    teste

--- Codigos/TPs/tp01/1_PalidromoJava/Palindromo.java
+++ Codigos/TPs/tp01/1_PalidromoJava/Palindromo.java
@@ -8,7 +8,7 @@
  * Crie um método iterativo que recebe uma string como parâmetro e retorna true
  * se essa é um palíndromo. Na saída padrão, para cada linha de entrada, escreva
  * uma linha de saída com SIM / NAO indicando se a linha é um palíndromo.
- * Destaca-se que uma linha de entrada pode ter caracteres não letras.
+ * Destaca-se que uma linha de entrada pode ter caracteres não letras. 
  */
 
 public class Palindromo{
