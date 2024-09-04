6fb1c1c861a42c848e5fe636864c932ff30d3848
cestpassion <bffonseca@sga.pucminas.br>
2024-09-04 10:00:02 -0300

    Programa - Lista

---
+++ Codigos/Programas/Lista.java
@@ -0,0 +1,33 @@
+public class Lista {
+    public static int n = 0;
+    public static int[] array = new int [6];
+
+    public static void inserirInicio(int num){
+        if(n > array.length)
+            
+    }
+
+    public static void inserirFim(int num){
+
+    }
+
+    public static void inserir(int num){
+
+    }
+
+    public static int removerInicio(){
+
+    }
+
+    public static int removerFim(){
+
+    }
+
+    public static int remover(int indice){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
