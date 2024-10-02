5c341b6fa160aba3a916d0ec2c9af225f6270531
cestpassion <bffonseca@sga.pucminas.br>
2024-10-02 00:55:43 -0300

    tp02

--- codigos/tps/tp02/TP02Q02_RegistroC/teste.c
+++ codigos/tps/tp02/TP02Q02_RegistroC/teste.c
@@ -1,8 +1,156 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
+//Registro Pokemon
+typedef struct Pokemon{
+    // Atributos
+    int id;
+    int generation;
+    char *name;
+    char *description;
+    char **types;
+    char **abilities;
+    double weight;
+    double height;
+    int captureRate;
+    char *isLegendary;
+    char *captureDate;
+} Pokemon;
+
+Pokemon *pokemon;
+int totalPokemons = 0;
+
+void ler();
+void imprimir(int);
+int get(int);
+// Pokemon set();
+// void save();
+// Pokemon clone(int);
+char* str(char*);
+int sizeStringsArray(char **);
+
+// Função principal --------------------------------------------------------------------------
 int main(){
-    bool teste = false;
-    printf("%d", teste);
+    pokemon = (Pokemon*)malloc(802 * sizeof(Pokemon));
+
+    ler();
+
+    char str[4];
+    int index;
+
+    while(scanf("%s", str) == 1 || strcmp(str, "FIM") != 0){
+        index = atoi(str);
+        imprimir(index);
+    }
+
+    // Liberar memória alocada
+    free(pokemon);
+    
+    return 0;
+}
+
+void ler(){
+    FILE *file = fopen("pokemon.csv", "r"); // /tmp/pokemon.csv
+    char buffer[1000];
+
+    fgets(buffer, sizeof(buffer), file);
+    while(totalPokemons <= 801){
+        fscanf(file, "%d%*c %d%*c", &pokemon[totalPokemons].id, &pokemon[totalPokemons].generation); // id, generation
+        fscanf(file, "%[^,]%*c", buffer); // name
+        pokemon[totalPokemons].name = str(buffer);
+        fscanf(file, "%[^,]%*c", buffer); // description
+        pokemon[totalPokemons].description = str(buffer);
+
+        // types
+        fscanf(file, "%[^\"]%*c", buffer);
+        char *pch = strtok(buffer, ",");
+        pokemon[totalPokemons].types = (char**)malloc(1 * sizeof(char*));
+        
+        int count = 0;
+        while(pch != NULL){
+            if(count >= 1)
+                pokemon[totalPokemons].types = (char**)realloc(pokemon[totalPokemons].types, (count + 1) * sizeof(char*));
+
+            pokemon[totalPokemons].types[count] = str(pch);
+            pch = strtok(NULL, ",");
+            count++;
+        }
+
+        // abilities
+        fscanf(file, "%[^\"]%*c%*c", buffer);
+        pch = strtok(buffer, " ,[]'");
+        pokemon[totalPokemons].abilities = (char**)malloc(1 * sizeof(char*)); 
+        
+        count = 0;
+        while(pch != NULL){
+            if(count >= 1)
+                pokemon[totalPokemons].abilities = (char**)realloc(pokemon[totalPokemons].abilities, (count + 1) * sizeof(char*));
+
+            pokemon[totalPokemons].abilities[count] = str(pch);
+            pch = strtok(NULL, " ,[]'");
+            count++;
+        }
+
+        fscanf(file, "%[^,]%*c", buffer); // weight
+        char *endptr;
+        pokemon[totalPokemons].weight = buffer != "" ? strtod(buffer, &endptr) : 0;
+        fscanf(file, "%[^,]%*c", buffer); // height
+        pokemon[totalPokemons].height = buffer != "" ? strtod(buffer, &endptr) : 0;
+        fscanf(file, "%d%*c", &pokemon[totalPokemons].captureRate); // captureRate
+        fscanf(file, "%[^,]%*c", buffer); // isLegendary
+        pokemon[totalPokemons].isLegendary = buffer == "0" ? str("false") : str("true");
+        fscanf(file, "%[^\n]", buffer); // captureDate
+        pokemon[totalPokemons].captureDate = str(buffer);
+        
+        totalPokemons++;
+    }
+}
+
+// Função imprimir ---------------------------------------------------------------------------
+void imprimir(int searchId){
+    int index = get(searchId);
+
+    printf("[#%d -> %s: %s - ", pokemon[index].id, pokemon[index].name, pokemon[index].description);
+
+    int numTypes = sizeStringsArray(pokemon[index].types);
+    printf("['%s'", pokemon[index].types[0]);
+    for(int i = 1; i < numTypes; i++)
+        printf(", '%s'", pokemon[index].types[i]);
+
+    printf("] - ");
+
+    int numAbilities = sizeStringsArray(pokemon[index].abilities);
+    printf("['%s'", pokemon[index].abilities[0]);
+    for(int i = 1; i < numAbilities - 1; i++)
+        printf(", '%s', ", pokemon[index].abilities[i]);
+
+    printf("'%s'] - ", pokemon[index].abilities[numAbilities - 1]);
+
+    printf("%.1lfkg - %.1lfm - %d - %s - %d gen] - %s\n", pokemon[index].weight, pokemon[index].height,
+    pokemon[index].captureRate, pokemon[index].isLegendary, pokemon[index].generation, pokemon[index].captureDate);
+}
+
+// Função get --------------------------------------------------------------------------------
+int get(int searchId){
+    for(int i = 0; i < totalPokemons; i++)
+        if(searchId == pokemon[i].id) { return i; }
+}
+
+// Função para alocar string -----------------------------------------------------------------
+char* str(char *str){
+    char *info = (char*)malloc((strlen(str) + 1) * sizeof(char));
+    strcpy(info, str);
+
+    return info;
+}
+
+// Função para encontrar o tamanho de um array de strings ------------------------------------
+int sizeStringsArray(char **array){
+    int size = 0;
+    
+    while (array[size] != NULL)
+        size++;
+    
+    return size;
 }
